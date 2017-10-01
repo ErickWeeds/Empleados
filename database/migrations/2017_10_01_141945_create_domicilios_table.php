ede4ae359d196e973dd2c807dbb86873cffb92cd
Erick <esandoval@arete.ws>
2017-10-01 09:25:16 -0500

    Migraciones y Modelos listos

---
+++ database/migrations/2017_10_01_141945_create_domicilios_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDomiciliosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('domicilios',function(Blueprint $table){
+            $table->increments('id');
+            $table->string('d_calle');
+            $table->integer('d_numero');
+            $table->string('d_interior');
+            $table->string('d_colonia');
+            $table->string('d_cp');
+            $table->string('d_estado');
+            $table->string('d_pais');
+            $table->integer('empleado_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('empleado_id')->references('id')->on('empleados')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('domicilios');
+    }
+}
